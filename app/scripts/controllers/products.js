cae9970a73baa5c41eb41136171732e47c053bb8
Abhas Mittal <mittalabhas1@gmail.com>
2014-10-03 10:39:03 +0530

    feat(products): made basic setup files

---
+++ app/scripts/controllers/products.js
@@ -0,0 +1,19 @@
+/*
+ * Author: @bhas (mittalabhas1@gmail.com) and V@m$he (vamshedhar@gmail.com)
+*/
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spoutinginApp.controller:ProductCtrl
+ * @description
+ * # ProductCtrl
+ * Controller of the spoutinginApp
+ */
+angular.module('spoutinginApp')
+  .controller('ProductCtrl', ['$scope', function ($scope) {
+
+    $scope.temps = {};
+
+  }]);
