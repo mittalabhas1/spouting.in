f63615e763d12e824994b2e406d434957d745df7
Abhas Mittal <mittalabhas1@gmail.com>
2014-10-14 23:47:30 +0530

    feat(products): added/edited categories and products

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -19,11 +19,6 @@ angular.module('spoutinginApp')
     $rootScope.global.products = {
       'category': [
         {
-          'name': 'Spouting',
-          'keyword': 'spouting',
-          'code': 'CG10',
-        },
-        {
           'name': 'Plantsifter Accessories',
           'keyword': 'plantsifter-accessories',
           'code': 'CG01',
@@ -39,19 +34,14 @@ angular.module('spoutinginApp')
           'code': 'CG03',
         },
         {
-          'name': 'Knobs & Handles',
-          'keyword': 'knobs-and-handles',
-          'code': 'CG05',
-        },
-        {
-          'name': 'Driving Elements',
-          'keyword': 'driving-elements',
-          'code': 'CG07',
-        },
-        {
           'name': 'Elevator & Conveyor Spares',
           'keyword': 'elevator-and-conveyor-spares',
           'code': 'CG04',
+        },
+        {
+          'name': 'Knobs & Handles',
+          'keyword': 'knobs-and-handles',
+          'code': 'CG05',
         },
         {
           'name': 'Pulse Jet Bag Filter Accessories',
@@ -59,10 +49,20 @@ angular.module('spoutinginApp')
           'code': 'CG06',
         },
         {
-          'name': 'Miscellaneous',
-          'keyword': 'miscellaneous',
+          'name': 'Vibration Absorbers',
+          'keyword': 'vibration-absorbers',
+          'code': 'CG08',
+        },
+        {
+          'name': 'Miller Tools',
+          'keyword': 'miller-tools',
+          'code': 'CG09',
+        },
+        {
+          'name': 'Spouting & Ducting',
+          'keyword': 'spouting-and-ducting',
           'code': 'CG10',
-        }
+        },
       ],
       'plantsifter-accessories': {
         'name': 'Plantsifter Accessories',
@@ -92,12 +92,6 @@ angular.module('spoutinginApp')
             'code': 'PS04',
           },
           {
-            'name': 'Plantsifter Box',
-            'keyword': 'plantsifter-box',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
             'name': 'Plantsifter Bag',
             'keyword': 'plantsifter-bag',
             'description': 'dsfsdfsdfsdf',
@@ -110,52 +104,58 @@ angular.module('spoutinginApp')
             'code': 'PS06',
           },
           {
+            'name': 'Plantsifter Box',
+            'keyword': 'plantsifter-box',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'PS07',
+          },
+          {
             'name': 'Plantsifter Oval Ball (Dholak)',
             'keyword': 'plantsifter-oval-ball-dholak',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS07',
+            'code': 'PS08',
           },
           {
             'name': 'Separator Ball',
             'keyword': 'separator-ball',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS08',
+            'code': 'PS09',
           },
           {
             'name': 'Staple Gun',
             'keyword': 'staple-gun',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS09',
+            'code': 'PS10',
           },
           {
             'name': 'Triangular Brush',
             'keyword': 'triangular-brush',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS10',
+            'code': 'PS11',
           },
           {
             'name': 'Sefar Nytal PA - MF',
             'keyword': 'sefar-nytal-pa-mf',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS11',
+            'code': 'PS12',
           },
           {
             'name': 'Sefar Nytal PA - Milling',
             'keyword': 'sefar-nytal-pa-milling',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS12',
+            'code': 'PS13',
           },
           {
             'name': 'Sefar Nytal PA - GG',
             'keyword': 'sefar-nytal-pa-gg',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS13',
+            'code': 'PS14',
           },
           {
             'name': 'Ilet Tape',
             'keyword': 'ilet-tape',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PS14',
+            'code': 'PS15',
           },
         ],
       },
@@ -163,10 +163,10 @@ angular.module('spoutinginApp')
         'name': 'Perforated Sheets',
         'products': [
           {
-            'name': 'Round Hole Type',
-            'keyword': 'round-hole-type',
+            'name': 'Dimple Type',
+            'keyword': 'dimple-type',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PRF03',
+            'code': 'PRF01',
           },
           {
             'name': 'Rectangular Hole Type',
@@ -175,16 +175,16 @@ angular.module('spoutinginApp')
             'code': 'PRF02',
           },
           {
+            'name': 'Round Hole Type',
+            'keyword': 'round-hole-type',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'PRF03',
+          },
+          {
             'name': 'Slotted Hole Type',
             'keyword': 'slotted-hole-type',
             'description': 'dsfsdfsdfsdf',
             'code': 'PRF04',
-          },
-          {
-            'name': 'Dimple Type',
-            'keyword': 'dimple-type',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'PRF01',
           },
           {
             'name': 'Triangular Hole Type',
@@ -193,28 +193,16 @@ angular.module('spoutinginApp')
             'code': 'PRF05',
           },
           {
-            'name': 'Stainless Steel Wire Mesh',
-            'keyword': 'stainless-steel-wire-mesh',
+            'name': 'Wire Mesh',
+            'keyword': 'wire-mesh',
             'description': 'dsfsdfsdfsdf',
-            'code': 'PRF01',
+            'code': 'PRF06',
           },
         ],
       },
       'pneumatic-accessories': {
         'name': 'Pneumatic Accessories',
         'products': [
-          {
-            'name': 'Air Lock',
-            'keyword': 'air-lock',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'PNU05',
-          },
-          {
-            'name': 'Acrylic Glass',
-            'keyword': 'acrylic-glass',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'PNU04',
-          },
           {
             'name': 'Lift Pipe',
             'keyword': 'lift-pipe',
@@ -234,6 +222,18 @@ angular.module('spoutinginApp')
             'code': 'PNU03',
           },
           {
+            'name': 'Lift Glass',
+            'keyword': 'lift-glass',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'PNU04',
+          },
+          {
+            'name': 'Air Lock',
+            'keyword': 'air-lock',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'PNU05',
+          },
+          {
             'name': 'Pneumatic Cyclone',
             'keyword': 'pneumatic-cyclone',
             'description': 'dsfsdfsdfsdf',
@@ -241,8 +241,233 @@ angular.module('spoutinginApp')
           },
         ],
       },
-      'spouting': {
-        'name': 'Spouting',
+      'elevator-and-conveyor-spares': {
+        'name': 'Elevator & Conveyor Spares',
+        'products': [
+          {
+            'name': 'Elevator Bucket',
+            'keyword': 'elevator-bucket',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC01',
+          },
+          {
+            'name': 'Belt',
+            'keyword': 'belt',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC02',
+          },
+          {
+            'name': 'Bolt',
+            'keyword': 'bolt',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC03',
+          },
+          {
+            'name': 'Bearings',
+            'keyword': 'bearings',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC04',
+          },
+          {
+            'name': 'Rubberized Pulley',
+            'keyword': 'rubberized-pulley',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC05',
+          },
+          {
+            'name': 'Elevator Top',
+            'keyword': 'elevator-top',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC06',
+          },
+          {
+            'name': 'Elevator Bottom',
+            'keyword': 'elevator-bottom',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC07',
+          },
+          {
+            'name': 'Elevator Box',
+            'keyword': 'elevator-box',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC08',
+          },
+          {
+            'name': 'Hanger',
+            'keyword': 'hanger',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC09',
+          },
+          {
+            'name': 'Conveyor Blades',
+            'keyword': 'conveyor-blades',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC10',
+          },
+          {
+            'name': 'Screw Flights',
+            'keyword': 'screw-flights',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC11',
+          },
+          {
+            'name': 'Conveyor Shaft Joint',
+            'keyword': 'conveyor-shaft-joint',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC12',
+          },
+          {
+            'name': 'Idler Roller',
+            'keyword': 'idler-roller',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC13',
+          },
+          {
+            'name': 'Drag Chain Conveyor Links',
+            'keyword': 'drag-chain-conveyor-links',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC14',
+          },
+          {
+            'name': 'Conveyor Window Clamps',
+            'keyword': 'conveyor-window-clamps',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'EC15',
+          },
+        ],
+      },
+      'knobs-and-handles': {
+        'name': 'Knobs & Handles',
+        'combined_products': true,
+        'products': [
+          {
+            'keyword': 'knobs1',
+            'code': 'KN01',
+          },
+          {
+            'keyword': 'knobs2',
+            'code': 'KN02',
+          },
+        ],
+      },
+      'pulse-jet-bag-filter-accessories': {
+        'name': 'Pulse Jet Bag Filter Accessories',
+        'products': [
+          {
+            'name': 'Filter Bags',
+            'keyword': 'filter-bags',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'BF01',
+          },
+          {
+            'name': 'Filter Cages with Ventury',
+            'keyword': 'filter-cages-with-ventury',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'BF02',
+          },
+          {
+            'name': 'Solenoid Valve',
+            'keyword': 'solenoid-valve',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'BF03',
+          },
+          {
+            'name': 'Sequential Timer Box',
+            'keyword': 'sequential-timer-box',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'BF04',
+          },
+          {
+            'name': 'FRL Unit',
+            'keyword': 'frl-unit',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'BF05',
+          },
+          {
+            'name': 'Pressure Guage',
+            'keyword': 'pressure-guage',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'BF06',
+          },
+        ],
+      },
+      'driving-elements': {
+        'name': 'Driving Elements',
+        'products': [
+          {
+            'name': 'Jaw Flex Coupling',
+            'keyword': 'jaw-flex-coupling',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'DE01',
+          },
+          {
+            'name': 'Pin Bush Coupling',
+            'keyword': 'pin-bush-coupling',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'DE02',
+          },
+          {
+            'name': 'Roll Body Differentials',
+            'keyword': 'roll-body-differentials',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'DE03',
+          },
+          {
+            'name': 'Sprocket',
+            'keyword': 'sprocket',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'DE04',
+          },
+          {
+            'name': 'Chain',
+            'keyword': 'chain',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'DE05',
+          },
+        ],
+      },
+      'vibration-absorbers': {
+        'name': 'Vibration Absorbers',
+        'products': [
+          {
+            'name': 'Vibration Pads',
+            'keyword': 'vibration-pads',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'VA01',
+          },
+          {
+            'name': 'Vibration Rubber',
+            'keyword': 'vibration-rubber',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'VA02',
+          },
+        ],
+      },
+      'miller-tools': {
+        'name': 'Miller Tools',
+        'products': [
+          {
+            'name': 'Flour Spetula',
+            'keyword': 'flour-spetula',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'MT01',
+          },
+          {
+            'name': 'Mesh Counter',
+            'keyword': 'mesh-counter',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'MT02',
+          },
+          {
+            'name': 'Measuring Tape',
+            'keyword': 'measuring-tape',
+            'description': 'dsfsdfsdfsdf',
+            'code': 'MT03',
+          },
+        ],
+      },
+      'spouting-and-ducting': {
+        'name': 'Spouting & Ducting',
         'products': [
           {
             'name': 'Pipe',
@@ -345,191 +570,6 @@ angular.module('spoutinginApp')
             'keyword': 'rotameter',
             'description': 'dsfsdfsdfsdf',
             'code': 'SP16',
-          },
-        ],
-      },
-      'elevator-and-conveyor-spares': {
-        'name': 'Elevator & Conveyor Spares',
-        'products': [
-          {
-            'name': 'Elevator Belting',
-            'keyword': 'elevator-belting',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC02',
-          },
-          {
-            'name': 'MS Bucket',
-            'keyword': 'ms-bucket',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
-            'name': 'PVC Buckets',
-            'keyword': 'pvc-buckets',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
-            'name': 'Elevator Bolt',
-            'keyword': 'elevator-bolt',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC02',
-          },
-          {
-            'name': 'Elevator Top',
-            'keyword': 'elevator-top',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC06',
-          },
-          {
-            'name': 'Elevator Bottom',
-            'keyword': 'elevator-bottom',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC07',
-          },
-          {
-            'name': 'Elevator Box',
-            'keyword': 'elevator-box',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC08',
-          },
-          {
-            'name': 'Rubberized Pulley',
-            'keyword': 'rubberized-pulley',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC05',
-          },
-          {
-            'name': 'Bearings',
-            'keyword': 'bearings',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC04',
-          },
-          {
-            'name': 'Screw Flights',
-            'keyword': 'screw-flights',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC11',
-          },
-          {
-            'name': 'Drag Chain Conveyor Links',
-            'keyword': 'drag-chain-conveyor-links',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC14',
-          },
-          {
-            'name': 'Conveyor Belting',
-            'keyword': 'conveyor-belting',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
-            'name': 'Bush Bearing',
-            'keyword': 'bush-bearing',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
-            'name': 'Hanger Bearing',
-            'keyword': 'hanger-bearing',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
-            'name': 'Conveyor Blades',
-            'keyword': 'conveyor-blades',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC10',
-          },
-          {
-            'name': 'Conveyor Inspection Window Clamps',
-            'keyword': 'conveyor-inspection-window-clamps',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
-            'name': 'Idler Roller',
-            'keyword': 'idler-roller',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'EC13',
-          },
-        ],
-      },
-      'pulse-jet-bag-filter-accessories': {
-        'name': 'Pulse Jet Bag Filter Accessories',
-        'products': [
-          {
-            'name': 'Filter Cages',
-            'keyword': 'filter-cages',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'BF02',
-          },
-          {
-            'name': 'Filter Bags',
-            'keyword': 'filter-bags',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'BF01',
-          },
-          {
-            'name': 'Solenoid Valve',
-            'keyword': 'solenoid-valve',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'BF03',
-          },
-          {
-            'name': 'Sequential Timer Box',
-            'keyword': 'sequential-timer-box',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'BF04',
-          },
-        ],
-      },
-      'driving-elements': {
-        'name': 'Driving Elements',
-        'products': [
-          {
-            'name': 'Jaw Flex Coupling',
-            'keyword': 'jaw-flex-coupling',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'DE01',
-          },
-          {
-            'name': 'Sprocket',
-            'keyword': 'sprocket',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'DE04',
-          },
-          {
-            'name': 'Star Coupling',
-            'keyword': 'star-coupling',
-            'description': 'dsfsdfsdfsdf',
-            'code': '',
-          },
-          {
-            'name': 'Pin Bush Coupling',
-            'keyword': 'pin-bush-coupling',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'DE02',
-          },
-          {
-            'name': 'Roll Body Gears',
-            'keyword': 'roll-body-gears',
-            'description': 'dsfsdfsdfsdf',
-            'code': 'DE03',
-          },
-        ],
-      },
-      'knobs-and-handles': {
-        'name': 'Knobs & Handles',
-        'combined_products': true,
-        'products': [
-          {
-            'keyword': 'knobs1',
-            'code': 'KN01',
-          },
-          {
-            'keyword': 'knobs2',
-            'code': 'KN02',
           },
         ],
       },
