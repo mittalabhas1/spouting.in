3acad84a5ad5e65f0f5a6a7c3eb4b61595a8c610
Abhas Mittal <mittalabhas1@gmail.com>
2014-11-09 16:34:44 +0530

    feat(theme): created theme, basic ui changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,10 +66,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 6625,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
-        livereload: 35729
+        hostname: '0.0.0.0',
+        livereload: 35728
       },
       livereload: {
         options: {
