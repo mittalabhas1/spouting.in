6bc23f15ee481a2c683874442d0a467c3d1e85cb
Abhas Mittal <mittalabhas1@gmail.com>
2014-11-16 03:19:16 +0530

    feat(home): added slider

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,8 +12,53 @@
  * Controller of the spoutinginApp
  */
 angular.module('spoutinginApp')
-  .controller('HomeCtrl', ['$scope', function ($scope) {
+  .controller('HomeCtrl', ['$scope', '$interval', function ($scope, $interval) {
 
     $scope.temps = {};
+    $scope.slides = ['slide1', 'slide2', 'slide3'];
 
-  }]);
+    $scope.currentIndex = 0;
+
+    $scope.setCurrentSlideIndex = function (index) {
+      $scope.currentIndex = index;
+    };
+
+    $scope.isCurrentSlideIndex = function (index) {
+      return $scope.currentIndex === index;
+    };
+
+    $scope.prevSlide = function () {
+      $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+    };
+
+    $scope.nextSlide = function () {
+      $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+    };
+
+    $interval(function(){
+      $scope.nextSlide();
+    }, 2000);
+
+  }])
+  .animation('.slide-animation', function () {
+    return {
+      addClass: function (element, className, done) {
+        if (className == 'ng-hide') {
+            TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
+        }
+        else {
+            done();
+        }
+      },
+      removeClass: function (element, className, done) {
+        if (className == 'ng-hide') {
+          element.removeClass('ng-hide');
+          TweenMax.set(element, { left: element.parent().width() });
+          TweenMax.to(element, 0.5, {left: 0, onComplete: done });
+        }
+        else {
+          done();
+        }
+      }
+    };
+  });
